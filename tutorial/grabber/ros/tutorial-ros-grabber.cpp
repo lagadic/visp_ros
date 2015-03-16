61434193cf720664e2c3b60ea5f705a7ce01cdff
Fabien Spindler <Fabien.Spindler@inria.fr>
2015-03-16 16:19:46 +0100

    Make the tutorial working with or whitout camera_info

--- tutorial/grabber/ros/tutorial-ros-grabber.cpp
+++ tutorial/grabber/ros/tutorial-ros-grabber.cpp
@@ -1,19 +1,43 @@
-/*! \example tutorial-ros-grabber.cpp */
+//! \example tutorial-ros-grabber.cpp
 #include <visp/vpDisplayX.h>
 #include <visp/vpImage.h>
 #include <visp_ros/vpROSGrabber.h>
 
-int main()
+int main(int argc, const char** argv)
 {
   try {
+    bool opt_use_camera_info = false;
+    for (int i=0; i<argc; i++) {
+      if (std::string(argv[i]) == "--use-camera-info")
+        opt_use_camera_info = true;
+      else if (std::string(argv[i]) == "--help") {
+        std::cout << "Usage: " << argv[0]
+                  << " [--use-camera-info] [--help]"
+                  << std::endl;
+        return 0;
+      }
+    }
+    std::cout << "Use camera info: " << ((opt_use_camera_info == true) ? "yes" : "no") << std::endl;
+
     //vpImage<unsigned char> I; // Create a gray level image container
     vpImage<vpRGBa> I; // Create a color image container
+    //! [Construction]
     vpROSGrabber g; // Create a grabber based on ROS
+    //! [Construction]
 
-    g.setCameraInfoTopic("/camera/camera_info");
+    //! [Setting camera topic]
     g.setImageTopic("/camera/image_raw");
-    g.setRectify(true);
+    //! [Setting camera topic]
+    //! [Setting camera info]
+    if (opt_use_camera_info) {
+      g.setCameraInfoTopic("/camera/camera_info");
+      g.setRectify(true);
+    }
+    //! [Setting camera info]
+
+    //! [Opening]
     g.open(I);
+    //! [Opening]
     std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
 
 #ifdef VISP_HAVE_X11
@@ -23,8 +47,11 @@ int main()
 #endif
 
     while(1) {
+      //! [Acquisition]
       g.acquire(I);
+      //! [Acquisition]
       vpDisplay::display(I);
+      vpDisplay::displayText(I, 20, 20, "A click to quit...", vpColor::red);
       vpDisplay::flush(I);
       if (vpDisplay::getClick(I, false))
         break;
