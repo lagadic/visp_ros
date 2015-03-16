64f13f0ed9f517babc2a91027aed80c113a3467b
Fabien Spindler <Fabien.Spindler@inria.fr>
2015-03-16 16:17:50 +0100

    Update ViSP examples for firewire and usb cameras

---
+++ tutorial/grabber/visp/tutorial-visp-grabber-v4l2.cpp
@@ -0,0 +1,34 @@
+//! \example tutorial-visp-grabber-v4l2.cpp
+#include <visp/vpV4l2Grabber.h>
+#include <visp/vpDisplayX.h>
+#include <visp/vpImage.h>
+
+int main()
+{
+#ifdef VISP_HAVE_V4L2
+  try {
+    vpImage<unsigned char> I;
+
+    vpV4l2Grabber g;
+    g.open(I);
+
+    std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
+
+#ifdef VISP_HAVE_X11
+    vpDisplayX d(I);
+#else
+    std::cout << "No image viewer is available..." << std::endl;
+#endif
+
+    while(1) {
+      g.acquire(I);
+      vpDisplay::display(I);
+      vpDisplay::flush(I);
+      if (vpDisplay::getClick(I, false)) break;
+    }
+  }
+  catch(vpException e) {
+    std::cout << "Catch an exception: " << e << std::endl;
+  }
+#endif
+}
